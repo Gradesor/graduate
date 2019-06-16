9ab953993ba676c0f0c8f268778faac733b46b97
Gradesor <heruvim694@gmail.com>
2019-06-17 04:04:43 +0500

    Сделал все правки куратора и сверстал одну секцию, подключил шрифтовые иконки

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,15 @@
-$('.nav-menu__btn').on('click', function(e){
-	e.preventDefault();
-	$(this).toggleClass('nav-menu__btn_active');
-})
+$(document).ready(function () {
+	$('.nav-menu__btn').on('click', function (e) {
+		e.preventDefault();
+		$(this).toggleClass('nav-menu__btn_active');
+	})
+	var button = $('.nav-item__btn');
+	var modal = $('.call');
+	var close = $('.call__close');
+	button.on('click', function () {
+		modal.addClass('call_active');
+	});
+	close.on('click', function () {
+		modal.removeClass('call_active');
+	});
+});
