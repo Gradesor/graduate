876561461955d4e0be16c15e1e2011a01bcbf1a4
Gradesor <heruvim694@gmail.com>
2019-06-18 01:34:35 +0500

    Сверстал секцию new, добавил ховеры ко всему только к чему можно, добавил маску для ввода телефона на мадалкебпо пахал с этими ховерами норм так

--- src/js/main.js
+++ src/js/main.js
@@ -12,4 +12,19 @@ $(document).ready(function () {
 	close.on('click', function () {
 		modal.removeClass('call_active');
 	});
+
+
+
+	$('.call-form__input').mask('8 (999) 999-99-99')
+
+
+
+	/* $('.card__item').mouseenter(function(){
+		$(this).children('.card').children('.card__box').css('display', 'flex');
+		$(this).children('.card').css('position', 'absolute');
+	});
+	$('.card__item').mouseleave(function () {
+		$(this).children('.card').children('.card__box').css('display', 'none');
+		$(this).children('.card').css('position', 'relative');
+	}); */
 });
